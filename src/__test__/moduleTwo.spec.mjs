6d05642192254d8b1ab5b8434cd66da2bc7fd902
Gustavo Gelpud Dagua <gustavo.gelpud@uao.edu.co>
2023-10-11 00:30:17 -0500

    Gustavo Gelpud Dagua 2201325

--- src/__test__/moduleTwo.spec.mjs
+++ src/__test__/moduleTwo.spec.mjs
@@ -3,33 +3,33 @@ import {
 } from '@jest/globals';
 import { generateFibonacciSequence, getUsers } from '../moduleTwo.mjs';
 
-describe('generateFibonacciSequence', () => {
-  test('generates Fibonacci sequence up to limit', () => {
+describe('generaSecuenciaFibonacci', () => {
+  test('genera secuencia de Fibonacci hasta el límite', () => {
     expect(generateFibonacciSequence(10)).toEqual([0, 1, 1, 2, 3, 5, 8]);
   });
 
-  test('throws an error for invalid limit', () => {
-    expect(() => generateFibonacciSequence(0)).toThrow('Limit must be greater than 0');
+  test('arroja un error por límite no válido', () => {
+    expect(() => generateFibonacciSequence(0)).toThrow('El límite debe ser mayor que 0');
   });
 });
 
 jest.mock('axios', () => ({
-  get: jest.fn(() => Promise.resolve({ data: [{ name: 'John' }, { name: 'Clementine' }] })),
+  get: jest.fn(() => Promise.resolve({ data: [{ name: 'Gustavo' }, { name: 'Claudia' }] })),
 }));
 describe('getUsers', () => {
-  test('fetches user data', async () => {
+  test('recupera datos del usuario', async () => {
     const users = await getUsers();
     expect(users.length).toBe(2);
   });
-  test('throws an error for no users found', async () => {
+  test('arroja error porque no encuentra usuarios', async () => {
     jest.spyOn(global.console, 'error').mockImplementation(() => {});
     jest.requireMock('axios').get.mockResolvedValueOnce({ data: [] });
-    await expect(getUsers()).rejects.toThrow('No users found');
+    await expect(getUsers()).rejects.toThrow('Usuario no encontrado');
   });
-  test('throws an error for Clementine in users', async () => {
+  test('arroja error para Claudia en usuarios', async () => {
     jest
       .spyOn(global.console, 'error').mockImplementation(() => {});
-    jest.requireMock('axios').get.mockResolvedValueOnce({ data: [{ name: 'Clementine' }] });
-    await expect(getUsers()).rejects.toThrow('Clementine is not allowed');
+    jest.requireMock('axios').get.mockResolvedValueOnce({ data: [{ name: 'Claudia' }] });
+    await expect(getUsers()).rejects.toThrow('Claudia no está permitida');
   });
 });
